d0af6bd7c44615b135d1f13d8b59c81f3646876c
Antonio Martinez <antonio_martinez88@hotmail.com>
2022-06-22 10:03:41 -0400

    updated

--- src/main.py
+++ src/main.py
@@ -41,5 +41,5 @@ def handle_hello():
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3000))
+    PORT = int(os.environ.get('PORT', 3340))
     app.run(host='0.0.0.0', port=PORT, debug=False)
