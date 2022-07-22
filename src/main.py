105805ea3d037cbd7d9dba52533975a9af6927de
Antonio Martinez <antonio_martinez88@hotmail.com>
2022-07-22 10:01:43 -0400

    changes to connect databse

--- src/main.py
+++ src/main.py
@@ -10,18 +10,35 @@ from .utils import APIException, generate_sitemap
 from .db import db
 from .admin import setup_admin
 
-from src.modelos import User #Aquí importamos los modelos registrador en el __init__.py de la carpeta modelos
+
+
 
 #from models import Person
-
+ENV = os.getenv("FLASK_ENV")
+#static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../public/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-MIGRATE = Migrate(app, db)
+#MIGRATE = Migrate(app, db)
+#db.init_app(app)
+##CORS(app)
+#setup_admin(app)
+##from src.rutas import enviarcorreo #Aquí importamos las rutas registradas en el __init__.py de la carpeta rutas
+
+# database condiguration
+DB_URL = os.getenv("DATABASE_URL")
+if DB_URL is not None:
+    app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL.replace("postgres://", "postgresql://")
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+MIGRATE = Migrate(app, db, compare_type = True)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
+from src.modelos import User #Aquí importamos los modelos registrador en el __init__.py de la carpeta modelos
 from src.rutas import enviarcorreo #Aquí importamos las rutas registradas en el __init__.py de la carpeta rutas
 
 # Handle/serialize errors like a JSON object
@@ -47,5 +64,5 @@ def handle_hello():
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3340))
+    PORT = int(os.environ.get('PORT', 5432))
     app.run(host='0.0.0.0', port=PORT, debug=False)
